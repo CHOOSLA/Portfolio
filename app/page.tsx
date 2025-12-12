3b43aa75f3537df1f0f6f56cf8d005fff1097787
Choo ChangWoo <cnckddn0146@gmail.com>
2025-12-12 21:09:40 +0900

    chore(Project) : 기존 예시 코드 초기화

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      테스트
-    </div>
-  );
-}
