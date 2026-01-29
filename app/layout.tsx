c796a3ca73ab0e68b7958847726ab33e298eb9ab
Choo ChangWoo <cnckddn0146@gmail.com>
2026-01-30 00:44:25 +0900

    feature(favicon) : favicon 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,9 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://www.about-changwoo.me",
   },
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
