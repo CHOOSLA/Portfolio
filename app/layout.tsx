5428b934445a9deb97e7c282939682a167bc12cc
Choo ChangWoo <cnckddn0146@gmail.com>
2026-01-31 01:11:51 +0900

    Refac(Perf) : 이미지 WebP로 최적화

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export const metadata: Metadata = {
     siteName: "ChooChangWoo Portfolio",
     images: [
       {
-        url: "/og-image.jpeg",
+        url: "/og-image.webp",
         width: 1200,
         height: 630,
         alt: "ChooChangWoo Portfolio",
@@ -64,7 +64,7 @@ export const metadata: Metadata = {
     title: "추창우 포트폴리오 :: CCW's Portfolio",
     description:
       "추창우 포트폴리오 웹사이트입니다. 기술을 도구로서 사용하는 본질을 탐구하는 개발자입니다.",
-    images: ["/og-image.jpeg"],
+    images: ["/og-image.webp"],
   },
   alternates: {
     canonical: "https://www.about-changwoo.me",
