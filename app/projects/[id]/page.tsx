b8002255462a69a3b5c08d73b7f02f2f71609811
Choo ChangWoo <cnckddn0146@gmail.com>
2026-01-31 01:04:58 +0900

    feature(SEO) : 프로젝트 상세 페이지 동적 메타데이터 생성 기능 추가

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 import { getAllProjects, getProjectById } from "@/lib/projects";
@@ -5,6 +6,42 @@ import ProjectDetail from "@/components/ProjectDetail";
 
 interface PageProps {
   params: Promise<{ id: string }>;
+}
+
+// 동적으로 메타 데이터를 생성하는 Nextjs 예약함수
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const resolvedParams = await params;
+  const project = getProjectById(Number(resolvedParams.id));
+
+  if (!project) {
+    return {
+      title: "페이지를 찾을 수 없습니다.",
+      description: "요청하신 프로젝트 상세 페이지를 찾을 수 없습니다.",
+    };
+  }
+
+  return {
+    title: project.title,
+    description: project.description,
+    openGraph: {
+      title: project.title,
+      description: project.description,
+      images: [
+        {
+          url: project.thumbnail,
+          alt: project.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: project.title,
+      description: project.description,
+      images: [project.thumbnail],
+    },
+  };
 }
 
 // 정적 콘텐츠 생성을 위한 함수
